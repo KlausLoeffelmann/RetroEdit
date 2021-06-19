b0cfe7737d89c8c225f6e7757e58872857fccaa0
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2021-06-19 12:05:22 -0700

    Edit make files.

--- src/Make.bat
+++ src/Make.bat
@@ -1,15 +1,21 @@
-set target=cx16
+REM set target=cx16
+set target=c64
 
-%onedriveconsumer%\C64\Dev\cc65\bin\cc65 -Orsi -t %target% screentools.c >screentools_cc_output.txt
-%onedriveconsumer%\C64\Dev\cc65\bin\cc65 -Orsi -t %target% retroeditmain.c >retroeditmain_cc_output.txt
+set vice128emu="%onedriveconsumer%\ClassicComputing\C64\WinVice (GTK)\GTK3VICE-3.5-win64\bin\x128"
+set vice64emu="%onedriveconsumer%\ClassicComputing\C64\WinVice (GTK)\GTK3VICE-3.5-win64\bin\x64sc"
+set cx16emu="%onedriveconsumer%\ClassicComputing\C-X16\x16emu"
+set cc65="%onedriveconsumer%\ClassicComputing\cc65"
+set cc65=%cc65:"=%
+
+%cc65%\bin\cc65 -Orsi -t %target% screentools.c >screentools_cc_output.txt
+%cc65%\bin\cc65 -Orsi -t %target% retroeditmain.c >retroeditmain_cc_output.txt
+
 del screentools.asm
 del retroeditmain.asm
 rename screentools.s screentools.asm
 rename retroeditmain.s retroeditmain.asm
 
-%onedriveconsumer%\C64\Dev\cc65\bin\cl65 -Orsi -t %target% screentools.c retroeditmain.c -o retroeditmain >retroeditmain_cl_output.txt -Wl "--mapfile,retroeditmain.map" -Wl "--dbgfile,retroeditmain.dbg"
+%cc65%\bin\cl65 -Orsi -t %target% screentools.c retroeditmain.c -o retroeditmain >retroeditmain_cl_output.txt -Wl "--mapfile,retroeditmain.map" -Wl "--dbgfile,retroeditmain.dbg"
+
 del RETROEDIT.PRG
 rename retroeditmain RETROEDIT.PRG
-copy RETROEDIT.PRG %onedriveconsumer%\C64\C-X16\Emulator\RETROEDIT.PRG
-
-
--- src/MakeAndRun.bat
+++ src/MakeAndRun.bat
@@ -1,20 +1,29 @@
 REM set target=cx16
 set target=c64
 
-%onedriveconsumer%\C64\Dev\cc65\bin\cc65 -Orsi -t %target% screentools.c >screentools_cc_output.txt
-%onedriveconsumer%\C64\Dev\cc65\bin\cc65 -Orsi -t %target% retroeditmain.c >retroeditmain_cc_output.txt
+set vice128emu="%onedriveconsumer%\ClassicComputing\C64\WinVice (GTK)\GTK3VICE-3.5-win64\bin\x128"
+set vice64emu="%onedriveconsumer%\ClassicComputing\C64\WinVice (GTK)\GTK3VICE-3.5-win64\bin\x64sc"
+set cx16emu="%onedriveconsumer%\ClassicComputing\C-X16\x16emu"
+set cc65="%onedriveconsumer%\ClassicComputing\cc65"
+set cc65=%cc65:"=%
+
+%cc65%\bin\cc65 -Orsi -t %target% screentools.c >screentools_cc_output.txt
+%cc65%\bin\cc65 -Orsi -t %target% retroeditmain.c >retroeditmain_cc_output.txt
+
 del screentools.asm
 del retroeditmain.asm
 rename screentools.s screentools.asm
 rename retroeditmain.s retroeditmain.asm
 
-%onedriveconsumer%\C64\Dev\cc65\bin\cl65 -Orsi -t %target% screentools.c retroeditmain.c -o retroeditmain >retroeditmain_cl_output.txt -Wl "--mapfile,retroeditmain.map" -Wl "--dbgfile,retroeditmain.dbg"
+%cc65%\bin\cl65 -Orsi -t %target% screentools.c retroeditmain.c -o retroeditmain >retroeditmain_cl_output.txt -Wl "--mapfile,retroeditmain.map" -Wl "--dbgfile,retroeditmain.dbg"
+
 del RETROEDIT.PRG
 rename retroeditmain RETROEDIT.PRG
-copy RETROEDIT.PRG %onedriveconsumer%\C64\C-X16\Emulator\RETROEDIT.PRG
 
 if "%target%"=="cx16" (
-%onedriveconsumer%\C64\C-X16\Emulator\x16emu.exe -prg X16EDITMAIN.PRG -run
+    %cx16emu% -prg RETROEDIT.PRG -run
 )
 
-
+if "%target%"=="c64" (
+    %vice64emu% RETROEDIT.PRG
+)
